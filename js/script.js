5443796f8a0772fb574359640dac334bf1bde67c
swergas <swergas@yahoo.fr>
2013-12-11 09:06:20 +0100

    Remplacement du bandeau de droite des pages 'Accueil' et 'Le constat' par une liste déroulante. https://www.loomio.org/discussions/9180?proposal=5669

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(function() {
 	
 	$( ".accordion" ).accordion( { collapsible: true, active: false, heightStyle: "content" } );
-	
+
+	$(".quick_explanation_video_form").on('submit', function(e)
+	{
+		var video_url = $('.quick_explanation_video_form .quick_explanation_video_time_value').val();
+		window.open(video_url);
+		return false;
+	});
 });
